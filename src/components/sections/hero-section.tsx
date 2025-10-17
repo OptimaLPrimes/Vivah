c01572d30d5c4e4ad8b7e1354fd563539db60798
optimal primes <primesoptimal@gmail.com>
2025-10-17 10:23:41 +0000

    change countdown style Flip Clock Style: Use a vintage flip clock animat

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { StylishCountdown } from "../stylish-countdown";
+import { CountdownTimer } from "../countdown-timer";
 import { AnimatedName } from "../animated-name";
 
 export function HeroSection() {
@@ -25,7 +25,7 @@ export function HeroSection() {
         </p>
 
         <div className="animate-breathe">
-            <StylishCountdown date={weddingDate} />
+            <CountdownTimer date={weddingDate} />
         </div>
 
         <div className="flex gap-4 mt-12">
