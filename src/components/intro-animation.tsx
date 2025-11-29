611c4869484f9dbde9cc27383e0a48f2b2075872
optimal primes <primesoptimal@gmail.com>
2025-11-29 20:07:05 +0000

    replace the intro idea with Concept: A classic paisley motif, similar to

--- src/components/intro-animation.tsx
+++ src/components/intro-animation.tsx
@@ -1,12 +1,13 @@
+
 "use client";
 
-import { LotusIcon } from "./icons/lotus-icon";
+import { PaisleyIcon } from "./icons/paisley-icon";
 
 export const IntroAnimation = () => {
   return (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-background animate-fade-out-intro">
       <div className="relative flex items-center justify-center">
-        <LotusIcon className="text-primary/70" />
+        <PaisleyIcon className="w-64 h-64 text-primary/70 animate-draw-in" />
         <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 animate-reveal-names">
           <h1 className="font-headline text-4xl text-primary-foreground animate-glow-text-soft">
             Vaishnavi
